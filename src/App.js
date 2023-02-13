e4a2f589322764e6751aff19e93fa49b00659c3c
summmermin <110873921+summmermin@users.noreply.github.com>
2023-02-13 17:59:00 +0900

    pages 생성 (react-router/ Component)

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {Routes, Route, Link} from 'react-router-dom'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Counter from "./pages/Counter";
+import  Input from "./pages/Input"
+import  Input2 from "./pages/Input2"
+import List from "./pages/List";
+import Customer from "./pages/Customer";
+
+const customer={
+    'name':'summer',
+    'birthday':'10/10',
+    'gender':'f',
+    'job':'developer'
+}
+
+function App() {
+    return (
+        <div className="App">
+            <nav>
+                <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link> | <Link to="/input">Input</Link> | <Link to="/input2">Input2</Link> | <Link to="/list">List</Link> | <Link to="/customer">Customer</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/counter" element={<Counter/>}/>
+                <Route path="/input" element={<Input/>}/>
+                <Route path="/input2" element={<Input2/>}/>
+                <Route path="/list" element={<List/>}/>
+                <Route path="/customer" element={            <Customer
+                    name={customer.name}
+                    gender={customer.gender}
+                    job={customer.job}
+                    birthday={customer.birthday}
+                />}/>
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+/*최근에는 이와같은 함수형 컴포넌트로 많이 개발하는 추세*/
+import React from "react";
+
+function Home(){
+    return <h1>home 화면입니다.</h1>
+}
+
+export default Home;
+
+// import React, {Component} from "react";
+//
+// class Home extends Component{
+//     render() {
+//         return <h1>Home 화면입니다.</h1>
+//     }
+// }
+//
+// export default Home;
+
+
+
