7cf982a53b7bcf015501739c75c7bed02e70f3d1
summmermin <110873921+summmermin@users.noreply.github.com>
2023-02-14 15:13:51 +0900

    fetch 이용하여 프론트에서 서버에 데이터 요청 완료

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -3,19 +3,26 @@ import React, {useEffect, useState} from "react";
 function TodoList() {
     const [todoList, setTodoList] = useState(null);
 
-//처음 컴포넌트 렌더링 될 때만 실행되도록 하기
-    useEffect(() => {
+    const fetchData = () => {
         fetch('http://localhost:8080/api/todo')
             .then((res) => res.json())
             .then((data) => setTodoList(data));
-    }, [])
+    }
+
+//처음 컴포넌트 렌더링 될 때만 실행되도록 하기
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
         const text = e.target.text.value;
-        const done = e.target.text.value;
+        const done = e.target.text.checked;
         fetch('http://localhost:8080/api/todo', {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
             body: JSON.stringify({
                 text,
                 done,
@@ -32,13 +39,12 @@ function TodoList() {
                 <input type="submit" value="추가"/>
             </form>
             {todoList?.map((todo) => (
-                <div key={todo.id}> {/*map 을 돌리니까 key 값이 필요!*/}
+                <div key={todo.id} style={{display: "flex"}}> {/*map 을 돌리니까 key 값이 필요!*/}
                     <div>{todo.id}</div>
                     <div>{todo.text}</div>
                     <div>{todo.done ? 'Y' : 'N'}</div>
                 </div>
             ))}
-
         </div>
     )
 }
